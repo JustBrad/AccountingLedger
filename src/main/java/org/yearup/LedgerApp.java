55e038b6e3e468f5890a39627882453b1bb2301e
Brad <bradegbert26@gmail.com>
2023-05-01 08:52:37 -0500

    Made basic Home screen outline

--- src/main/java/org/yearup/LedgerApp.java
+++ src/main/java/org/yearup/LedgerApp.java
@@ -6,8 +6,43 @@ public class LedgerApp
 {
     static Scanner scanner = new Scanner(System.in);
 
+    // Display Home screen
+    public void displayHome()
+    {
+        System.out.println("\n----------HOME-SCREEN----------\n");
+        System.out.println("What would you like to do?\n");
+        System.out.println("D) Add Deposit");
+        System.out.println("P) Make Payment (Debit)");
+        System.out.println("L) Display Ledger Screen");
+        System.out.println("X) Exit\n");
+
+        System.out.print("Enter an option: ");
+        String option = scanner.nextLine().toUpperCase();
+
+        while(true)
+        {
+            switch(option)
+            {
+                case "D":
+                    // Add deposit
+                    break;
+                case "P":
+                    // Make payment
+                    break;
+                case "L":
+                    // Display Ledger screen
+                    break;
+                case "X":
+                    System.out.println("\nExiting...");
+                    return;
+                default:
+                    System.out.println("\nInvalid option.");
+            }
+        }
+    }
+
     public void run()
     {
-        
+        displayHome();
     }
 }
